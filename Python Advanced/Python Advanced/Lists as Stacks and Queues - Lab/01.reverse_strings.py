bf3667c462d6c5c9465e9879e8ce8448aae83844
Viktor Stanimirov <122106162+viktorstanimirov@users.noreply.github.com>
2023-11-13 11:33:57 +0200

    Delete Python Advanced/Python Advanced directory

--- Python Advanced/Python Advanced/Lists as Stacks and Queues - Lab/01.reverse_strings.py
+++
@@ -1,8 +0,0 @@
-string = (input())
-
-reversed_string = list(string)
-
-
-while reversed_string:
-    removed_chars = reversed_string.pop()
-    print(removed_chars, end='')
--- Python Advanced/Python Advanced/Tuples and Sets - Exercise/sets_of_elements.py
+++
@@ -1,7 +0,0 @@
-set_a_numbers, set_b_numbers = [int(x) for x in input().split()]
-
-first_set_nums = {int(input()) for _ in range(set_a_numbers)}
-second_set_nums = {int(input()) for _ in range(set_b_numbers)}
-
-
-print(*first_set_nums.intersection(second_set_nums), sep="\n")
