0260bc057b3b213688aa21fd6648624090e60ec9
Viktor Stanimirov <122106162+viktorstanimirov@users.noreply.github.com>
2023-11-13 11:44:07 +0200

    Add files via upload

---
+++ Python Advanced/Python OOP/First Steps in OOP/SOLID/01_workers.py
@@ -0,0 +1,90 @@
+from abc import ABC, abstractmethod
+
+
+class BaseWorker(ABC):
+    @staticmethod
+    @abstractmethod
+    def work():
+        pass
+
+
+class Worker(BaseWorker):
+    @staticmethod
+    def work():
+        print("I'm working!!")
+
+
+class SuperWorker(BaseWorker):
+    @staticmethod
+    def work():
+        print("I work very hard!!!")
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        if not isinstance(worker, BaseWorker):
+            raise AssertionError(f"`worker` must be of type {BaseWorker}")
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+
+
+worker = Worker()
+manager = Manager()
+manager.set_worker(worker)
+manager.manage()
+
+super_worker = SuperWorker()
+try:
+    manager.set_worker(super_worker)
+    manager.manage()
+except AssertionError:
+    print("manager fails to support super_worker....")
+
+# This is the code before using SOLID
+
+
+# class Worker:
+#
+#     def work(self):
+#         print("I'm working!!")
+#
+#
+# class Manager:
+#
+#     def __init__(self):
+#         self.worker = None
+#
+#     def set_worker(self, worker):
+#         assert isinstance(worker, Worker), '`worker` must be of type {}'.format(Worker)
+#
+#         self.worker = worker
+#
+#     def manage(self):
+#         if self.worker is not None:
+#             self.worker.work()
+#
+# class SuperWorker:
+#
+#     def work(self):
+#         print("I work very hard!!!")
+#
+#
+#
+# worker = Worker()
+# manager = Manager()
+# manager.set_worker(worker)
+# manager.manage()
+#
+# super_worker = SuperWorker()
+# try:
+#     manager.set_worker(super_worker)
+# except AssertionError:
+#     print("manager fails to support super_worker....")
